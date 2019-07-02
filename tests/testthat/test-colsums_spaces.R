0e5a95b4c973e4b7cc95d37fb9f900689b81cb1e
Your Name <you@example.com>
2019-07-02 20:13:14 +0000

    Now redefining the func in the test

--- tests/testthat/test-colsums_spaces.R
+++ tests/testthat/test-colsums_spaces.R
@@ -1,17 +1,15 @@
 test_that("colsums_spaces() returns text not error", {
-  #' Build a test table
+  # Build a test table
     test_table <- matrix(c(51,43,22,NA,28,21,68,'',9),ncol=3,byrow=TRUE)
     colnames(test_table) <- c("High","Low","Middle")
     test_table <- as.tibble(test_table)
-  #' Get test_table_results
-    test_table_restults <- colSums(test_table == "")
-
-  colsums_spaces = function(table_name){
-    results = colSums(table_name == "")
-    return(results)}
+  # Get test_table_results
 
 
-  #expect_equal(results, test_table_restults)
+    test_table_results = colsum_spaces(test_table)
+
+
+  expect_equal(results, test_table_results)
 })
 
 
